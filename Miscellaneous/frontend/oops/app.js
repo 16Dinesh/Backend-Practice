9c04921e1a572396f924f6c5dae9175f828db27c
Dinesh R <144953120+16Dinesh@users.noreply.github.com>
2024-05-28 19:06:21 +0530

    Update app.js

---
+++ Miscellaneous/frontend/oops/app.js
@@ -0,0 +1,35 @@
+// let arr = [1,2,4,5];
+// let arr2 = [1,2,4,5];  //prototype funtions exits in every array
+// arr.sayhello = () => {
+//     console.log(`this is an Array`);   // just to check the prototype 
+// }
+// arr2.sayhello = () => {
+//     console.log(`this is an Array`);   
+// }
+
+//factory functions
+
+//template
+//     person {
+//     name : "this",
+//     age : 2;
+// }
+
+// function PersonMaker (name , age) {
+//     const person = {
+//         name : name,
+//         age : age,
+//         talk() {
+//             console.log(`This is my name ${this.name} \n and my age : ${this.age}`);
+//         },
+//     };
+//     return person;
+// };
+
+// let p1 = PersonMaker("Dinesh" , 21);
+// let p2 = PersonMaker("Varsha" , 21);
+// let d3 = PersonMaker("Brutu" , 2);
+
+
+
+// new Operator
