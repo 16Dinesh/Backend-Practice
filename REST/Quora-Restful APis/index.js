4498818d076826d0a56607a90fdb75fd41caeae1
test <Dineshwonks@Gmail.com>
2024-05-30 22:16:08 +0530

    create and update

--- REST/Quora-Restful APis/index.js
+++ REST/Quora-Restful APis/index.js
@@ -7,19 +7,22 @@ app.use(express.urlencoded({extended : true}));
 
 app.set("view engine" , "ejs");
 app.set("views",path.join(__dirname,"views"))
-app.use(express.static(path.join(__dirname,"public/css"))); 
-
+app.use(express.static(path.join(__dirname,"public/"))); 
+``
 
 let posts = [
     {
+      id: "1a",
       username: "Dinesh",
       content: "Learning MERN"
     },
     {
+      id: "1b",
       username: "Varsha",
       content: "Learning DSA"
     },
     {
+      id: "1c",
       username: "Brutu",
       content: "I'm just a Dog"
     }
@@ -27,7 +30,7 @@ let posts = [
   
 
 app.get("/posts" , (req , res) => {
-    res.render("index" , {posts})
+    res.render("index.ejs" , {posts})
 })
 
 app.get("/posts/new" , (req , res) => {
@@ -35,9 +38,20 @@ app.get("/posts/new" , (req , res) => {
 })
 
 app.post("/posts" ,(req,res) => {
-    console.log(req.body);
-    res.send("post req is working");
-})
+    let {username , content} = req.body;
+    posts.push({ username , content }) // adds the data to posts
+    // res.send("post req is working");
+    res.redirect("/posts")  //by default  sends the get request
+});
+
+app.get("/posts/:id" , (req,res)=> {
+  let {id} = req.params;
+  // console.log(id);  // working
+  let post = posts.find((p) => id === p.id); // Finding the post with the matching 'id'
+  // console.log(post); //working 
+  // res.send("request working")
+  res.render("show.ejs" , {post});
+});
 
 app.listen(port , () => {
     console.log(`listing to the port : ${port}`);
