a92991d3e698ecf63e552a736943500963724e29
test <Dineshwonks@Gmail.com>
2024-06-18 13:13:20 +0530

    Setup of ChatModel

--- DataBases-Learning/Mongodb/Mongo-Express/index.js
+++ DataBases-Learning/Mongodb/Mongo-Express/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine" , "ejs");
+app.use(express.static(path.join(__dirname, "public")))
+
 
 main()
     .then(() => {
@@ -16,6 +19,14 @@ main()
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/chat');
 }
+
+//Index Route
+app.get("/chats", async (req,res) => {
+    let chats = await Chat.find()
+    // console.log(chats);
+    res.render("index" , {chats});
+});
+
 
 app.get("/" , (req,res)=>{
     res.send("working")
