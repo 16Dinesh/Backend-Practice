dd8f2bc9ee7d503ba96856de30039df212a3a8ce
test <Dineshwonks@Gmail.com>
2024-06-18 15:03:36 +0530

    chat model

--- DataBases-Learning/Mongodb/Mongo-Express/index.js
+++ DataBases-Learning/Mongodb/Mongo-Express/index.js
@@ -3,10 +3,13 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine" , "ejs");
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended : true}));
+app.use(methodOverride("_method"));
 
 
 main()
@@ -27,6 +30,47 @@ app.get("/chats", async (req,res) => {
     res.render("index" , {chats});
 });
 
+//New Route
+app.get("/chats/new", (req,res)=>{
+    res.render("new");
+})
+
+//Create Route 
+app.post("/chats" ,(req,res)=>{
+    let {from , to , msg } = req.body;
+    let newChat = new Chat ({
+        from: from,
+        to: to,
+        msg : msg,
+        created_at: new Date(),
+    });
+    newChat.save().then((res)=>{console.log("Data is passed")}).catch((err)=>{console.log(err)});
+    res.redirect("/chats")
+});
+
+//Edit Route
+app.get('/chats/:id/edit' , async (req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit" , {chat});
+})
+
+//UPDATE Route
+app.put("/chats/:id" , async (req,res)=>{
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    let updatedChat = await Chat.findByIdAndUpdate(id , {msg : newMsg} , {runValidators: true , new: true});
+    console.log(updatedChat);
+    res.redirect("/chats");
+})
+
+//Destory route
+app.delete("/chats/:id" , async (req,res)=>{
+    let {id} = req.params;
+    let delChat = await Chat.findByIdAndDelete(id);
+    console.log(delChat);
+    res.redirect("/chats");
+})
 
 app.get("/" , (req,res)=>{
     res.send("working")
