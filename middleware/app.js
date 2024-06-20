7d4869c7cab0a5ccdbb19d0c89f0706eac3e86c6
test <Dineshwonks@Gmail.com>
2024-06-20 14:37:50 +0530

    middleware uses

--- middleware/app.js
+++ middleware/app.js
@@ -3,10 +3,55 @@ const app = express();
 
 //middleware -> response send 
 
-app.get("/", (req,res) => {
-    
+// app.use( (req,res,next)=> {
+//     console.log("Hi, this is middleware")
+//     next();
+// }) 
+
+// app.use(
+//     (req,res,next)=> {
+//     console.log("Hi, this is middleware")
+//      return next();
+//      console.log(`test`) // doen't work
+// })
+
+// app.use("/random", (req,res,next) => {
+//     console.log("this is for random MW");
+//     next();
+// })
+
+const checkToken = (req,res,next)=>{
+    let {token} = req.query;
+    if(token === "giveaccess"){
+        next()
+    }
+    res.send("Access Denied!");  
+};
+
+app.get("api", checkToken, (req,res)=> {
+    res.send("data")
 })
 
+app.get("/", (req,res) => {
+    res.send("Hi im root")
+})
+
+
+app. get ( "/random" , (req, res) => {
+    res.send("is a random page");
+})
+
+//logger
+// app.use( (req,res,next) => {
+//     req.time = new Date(Date.now()).toString();
+//     console.log(req.method, req.hostname, req.path, req.time);
+//     next();
+// })
+
+//404
+app.use((req,res) => {
+    res.send("Page not found!")
+})
 
 app.listen(8080 , () => {
     console.log(`app listening on port : 8080`);
