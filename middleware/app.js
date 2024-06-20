d9af51f2bd891e552c97fa9439cd4f957618bc81
test <Dineshwonks@Gmail.com>
2024-06-20 17:34:03 +0530

    middlewares

--- middleware/app.js
+++ middleware/app.js
@@ -25,7 +25,7 @@ const checkToken = (req,res,next)=>{
     if(token === "giveaccess"){
         next()
     }
-    res.send("Access Denied!");  
+    throw new Error("Access Denied!");  
 };
 
 app.get("api", checkToken, (req,res)=> {
