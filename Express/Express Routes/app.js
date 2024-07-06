8d141d66abe5c13460300ba95041bf0108372aec
test <Dineshwonks@Gmail.com>
2024-07-06 11:26:42 +0530

    Basic Cookies understanding

--- Express/Express Routes/app.js
+++ Express/Express Routes/app.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const app = express();
 const users = require("./routes/user");
-const posts = require("./routes/posts")
+const posts = require("./routes/posts");
+const cookieParser = require("cookie-parser");
 
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcookie", (req,res) => {
+    res.cookie("made-By", "Dinesh", { signed: true });
+    res.send("Send Signed Cookie")
+})
+
+app.get("/verify" ,(req,res) => {
+    //console.dir(req.cookies);     // for UnSignedCookies
+    console.log(req.signedCookies)  //for SignedCookies
+    res.send("verified");
+})
+
+app.get("/getcookies", (req,res) => {
+    res.cookie("This cookie", "active");
+    res.cookie("Greet", "Hello");
+    res.send('Sending some cookies');
+});
+
+app.get("/greet", (req,res) => {
+    let {name = "anonymous"} = req.cookies;
+    res.send(`Ho hoi , ${name}`)
+})
 
 app.get("/", (req,res) => {
+    console.dir(req.cookies);
     res.send("Hi, I am Root!");
-})
+});
 
 // users
 app.use("/users", users);
 //posts
 app.use("/posts", posts);
-
-
-
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
