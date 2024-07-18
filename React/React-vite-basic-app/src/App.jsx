f966d3410ecca6f9d6169abb63437b260e67b745
test <Dineshwonks@Gmail.com>
2024-07-18 20:46:40 +0530

    activity-1

--- React/React-vite-basic-app/src/App.jsx
+++ React/React-vite-basic-app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 //import Title from "./Title.jsx"
 //import {Title, Description,Text} from "./Title.jsx"
-import ProductList from "./productlist.jsx"
+import ProductList from "./productlist.jsx";
+import MsgBoxList  from './MsgBoxList.jsx'
 
 
 function App() {
   return (
     <>
+      <MsgBoxList/>
       <ProductList/>
     </>
   )
---
+++ React/React-vite-basic-app/src/MsgBoxList.jsx
@@ -0,0 +1,12 @@
+import MsgBox from './MsgBox.jsx';
+
+function MsgBoxList() {
+    return (
+        <div>
+            <MsgBox userName="Dinesh" textColor="Red"/>
+            <MsgBox userName="Varsha" textColor="Black"/>
+        </div>
+    );
+}
+
+export default MsgBoxList;
